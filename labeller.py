d3f409fecd8b470080b4595bcc23aa67afb5167f
Joseph Ribeiro (Cefas) <joseph.ribeiro@cefas.gov.uk>
2024-10-24 11:26:22 +0100

    broken state still...

--- labeller.py
+++ labeller.py
@@ -4,7 +4,6 @@ import os
 import json
 from PIL import Image, ImageTk
 import torch
-from utils import resnet18, get_device, classify, save_to_files, load_images_from_directory, bind_keys, setup_ui
 import time
 import pandas as pd
 
@@ -66,12 +65,6 @@ class COCOAnnotator(tk.Tk):
             data = json.load(json_file)
             labels_dict = data.get('labels', {})
             self.LABELS = {int(num): name for num, name in labels_dict.items()}        
-        arch = resnet18(num_classes=len(self.LABELS)).to(self.device)
-        arch.load_state_dict(torch.load("model_18_21May.pth", map_location=self.device))
-        self.model = arch
-        print("loaded example model and model labels")
-        print(self.LABELS)
-        print(self.model)
 
 
         # Default images
@@ -263,6 +256,106 @@ class COCOAnnotator(tk.Tk):
             messagebox.showerror("Save Failed", "Failed to save the file. Please check the save path and ensure the file is not already open.")
 
         save_to_files(self.data, self.labels_directory, self.output_name)
+
+
+def get_device():
+    device = torch.device("cpu")
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    return device
+
+def classify(image_path, device, model, labels):
+    image = tiff.imread(image_path)
+    t = functional.to_tensor(image)
+    t = functional.resize(t, (256, 256))
+    t = t.unsqueeze(dim=0)
+    t = t.to(device)
+    with torch.set_grad_enabled(False):
+        outputs = model(t)
+        scores = torch.softmax(outputs, dim=1)
+        _, preds = torch.max(outputs, 1)
+    class_number = preds[0].item()
+    class_name = labels.get(class_number, "Unknown")
+    return class_name, scores[0]
+
+
+
+def save_to_files(data, labels_directory, output_name):
+    try:
+        json_path = os.path.join(labels_directory, f"{output_name}.json")
+        with open(json_path, 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+
+        csv_path = os.path.join(labels_directory, f"{output_name}.csv")
+        with open(csv_path, 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            headers = ["file_name", "folder_name"] + list(data['image_fields'].keys()) + ["predicted_label"]
+            writer.writerow(headers)
+            for image in data["images"]:
+                row = [image.get(header, "") for header in headers]
+                writer.writerow(row)
+        return True
+    except Exception as e:
+        print(f"Failed to save files: {e}")
+        return False
+
+def bind_keys(app):
+    for i in range(1, 10):
+        app.bind(f"<Alt-Key-{i}>", app.focus_nth_entry)
+    app.bind("<Control-n>", lambda event: app.save_fields_and_next_image())
+    app.bind("<Control-s>", app.save_data)  # Bind Ctrl + S to save function
+
+
+
+
+def setup_ui(app):
+
+    app.select_labels_dir_button = tk.Button(app, text="1: Provide save output directory for labels (my/saved/outputs/dir)", command=app.select_labels_directory)
+    app.select_labels_dir_button.grid(row=2, column=0, columnspan=2, pady=5, sticky="ew")
+
+    app.select_dir_button = tk.Button(app, text="2: Provide input cyz", command=app.select_image_directory)
+    app.select_dir_button.grid(row=3, column=0, columnspan=2, pady=5, sticky="ew")
+
+    app.import_config_button = tk.Button(app, text="Optional: Import custom labelling standard", command=app.import_config)
+    app.import_config_button.grid(row=4, column=0, columnspan=2, pady=5, sticky="ew")
+
+    app.process_all_button = tk.Button(app, text="Optional: Process All", command=app.process_all_images)
+    app.process_all_button.grid(row=5, column=0, columnspan=2, pady=5, sticky="ew")
+
+    app.main_frame = tk.Frame(app)
+    app.main_frame.grid(row=6, column=0, columnspan=2, sticky="nsew")
+
+    app.canvas = tk.Canvas(app.main_frame, width=500, height=500)
+    app.canvas.grid(row=0, column=0, padx=5, pady=5)
+
+    app.fields_frame = tk.Frame(app.main_frame)
+    app.fields_frame.grid(row=0, column=1, padx=5, pady=5, sticky="ns")
+
+    app.next_button = tk.Button(app, text="Next Image", command=app.save_fields_and_next_image)
+    app.next_button.grid(row=7, column=0, columnspan=2, pady=5, sticky="ew")
+
+    app.data = {
+        "images": [],
+        "image_fields": app.image_fields
+    }
+
+
+
+def load_images_from_directory(image_directory):
+    valid_extensions = (
+        '.png', '.PNG', '.jpg', '.JPG', '.jpeg', '.JPEG', '.bmp', '.BMP', '.gif', '.GIF',
+        '.tiff', '.TIFF', '.tif', '.TIF', '.ico', '.ICO', '.webp', '.WEBP', '.svg', '.SVG',
+        '.heic', '.HEIC', '.heif', '.HEIF', '.jfif', '.JFIF', '.pjpeg', '.PJPEG', '.pjp', '.PJP', '.avif', '.AVIF'
+    )
+    
+    images = []
+    for dp, dn, filenames in os.walk(image_directory):
+        for f in filenames:
+            file_path = os.path.join(dp, f)
+            if f.lower().endswith(valid_extensions):
+                images.append(file_path)
+    
+    return images
 
 if __name__ == "__main__":
     app = COCOAnnotator()
