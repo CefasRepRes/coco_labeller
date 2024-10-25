5f21816ed33e879475bf92a43a3739c7a0027d18
Joseph Ribeiro (Cefas) <joseph.ribeiro@cefas.gov.uk>
2024-10-25 14:40:19 +0100

    splitting script up

--- cyz_processor.py
+++ cyz_processor.py
@@ -8,19 +8,7 @@ import csv
 import os
 import json
 import pandas as pd
-
-def load_json(file_path):
-    with open(file_path, 'r') as f:
-        json_data = json.load(f)
-    return json_data
-
-def select_particles(json_data, particle_ids):
-    particles = [p for p in json_data['particles'] if p['particleId'] in particle_ids]
-    return particles if particles else None
-
-def get_pulses(particles):
-    pulses = {p['particleId']: p.get('pulseShapes') for p in particles}
-    return pulses
+import functions
 
 class BlobApp:
     def __init__(self, root):
